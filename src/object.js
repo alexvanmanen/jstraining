046e104398a309922ea3b9fb74ad87d2125f0a41
Alex <alexvanmanen@hotmail.com>
2020-12-10 16:37:05 +0100

    object.js

--- src/object.js
+++ src/object.js
@@ -15,18 +15,36 @@ class Auto {
 
 console.log();
 
-let set = new Set();
-set.add(new Auto("Toyota", "Prius", "12-22-XN"));
-set.add(new Auto("Audi", "RS8", "12-23-XN"));
-set.add(new Auto("Tesla", "Model 3", "12-24-XN"));
-
-set.forEach(value => console.log(value));
+// const z = (a, b, c=1) => a + b * c;
+// const y = z(z(2,5, 5),z(3,4));
+// console.log(y);
 
 
-let map = new Map();
-map.set("Auto", "Car");
-map.set("Fiets", "Bike");
-map.set("Fiets", "Bicycle");
+const autos = [
+    {merk: 'Tesla', model: "Model 3"},
+    {merk: 'BMW', model: "515"},
+    {merk: 'Audi', model: "A8"},
+    {merk: 'Mazda', model: "MX-5"},
+    {merk: 'Toyota', model: "Celica"}
+];
+function print(value) {
+    console.log(value);
+}
 
-map.forEach((value, key) => console.log("value: "+ value + " key:" + key));
+var lijstMetModelNamen = autos.map(value => value.model);
+
+function isEenGaveBak(value, index, array) {
+    if(value === "MX-5" || value === "Celica") {
+        return value + " is een gave bak" + index + array;
+    }
+    return value + " is wel aardig" + index + array;
+}
+
+
+console.log(lijstMetModelNamen.map(isEenGaveBak));
+console.log(lijstMetModelNamen.map((value,index) => isEenGaveBak(value, index)));
+
+lijstMetModelNamen.forEach(modelNaam => console.log(modelNaam.length));
+// console.log(autos.map(value => value.model));
+
 
