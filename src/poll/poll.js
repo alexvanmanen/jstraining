1364758c92bb8e51135e693ebaa5927455d9720e
Remy Hersbach <remy.hersbach@nos.nl>
2020-12-22 13:33:37 +0100

    doet t

--- src/poll/poll.js
+++ src/poll/poll.js
@@ -1,29 +1,51 @@
-function laad(bestanden, doeIets) {
-
-    bestanden.forEach((bestand) => {
-            var xhttp = new XMLHttpRequest();
+function doeHet() {
+    return new Promise((resolve, reject) => {
+            let xhttp = new XMLHttpRequest();
 
             xhttp.onreadystatechange = function () {
-                if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-                    doeIets(JSON.parse(this.responseText));
-                }
-            }
-            xhttp.open("GET", bestand, true);
-            xhttp.send();
+                if (this.readyState === XMLHttpRequest.DONE) {
+                    if (this.status === 200) {
+                        resolve(JSON.parse(this.responseText));
+                    } else {
+                        reject('Oeps, er ging iets fout.');
+                    }
 
+                }
+
+
+            }
+        xhttp.open("GET", "results.json", true);
+        xhttp.send();
         }
     );
+}
 
+let promise1 = new Promise(function (resolve, reject) {
+    function laad(bestand) {
+        bestand = "results.json";
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                resolve(JSON.parse(this.responseText));
+            }
+        }
+
+        xhttp.open("GET", bestand, true);
+        xhttp.send();
+    }
+});
+
+function resolveNa4Seconds() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve('resolved 4 seconden');
+        }, 2000);
+    });
 }
 
 function showPoll(data) {
     data.forEach(parseData);
-}
-
-function calculateAverage(data) {
-    console.log(data);
-
-    // data.map(element => element.seats).reduce( ( p, c ) => p + c, 0 );
 }
 
 function parseData(result) {
@@ -39,11 +61,21 @@ function parseData(result) {
 
 }
 
-laad(['results.json', 'resultsAlphenadRijn.json', 'resultsUtrecht.json'], calculateAverage);
+async function render() {
+    const x = await doeHet();
+    console.log(x);
+    console.log(await resolveNa4Seconds());
+}
 
-const array = [[{ party: "VVD", seats: 34 }, { party: "CDA", seats: 1 }], { party: "VVD", seats: 12 }, {party:"CDA", seats: 12}];
-const mergedArray = array.flat(1);
-console.log(array);
-console.log(mergedArray);
-const average = (mergedArray) => mergedArray.map(element => element.seats).reduce((vorigeWaarde, huidigeWaarde) => vorigeWaarde + huidigeWaarde, 0) / mergedArray.length;
-array.forEach(xab => console.log(average(mergedArray.filter(element => element.party === xab.party))));
+render();
+
+const poll = [[{ party: "VVD", seats: 34 }, { party: "CDA", seats: 1 }], { party: "VVD", seats: 12 }, {
+    party: "CDA",
+    seats: 12
+}];
+const mergedArray = poll.flat(1);
+
+const average = (array) => array.map(element => element.seats).reduce((vorigeWaarde, huidigeWaarde) => vorigeWaarde + huidigeWaarde, 0) / array.length;
+mergedArray.forEach(partyInfo => {
+    console.log(average(mergedArray.filter(element => element.party === partyInfo.party)))
+});
